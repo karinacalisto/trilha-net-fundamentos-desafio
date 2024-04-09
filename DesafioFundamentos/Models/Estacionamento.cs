5afe0b7c73c81525b319cb21e2142c3b0d8f1772
Karina Calisto <42557017+karinacalisto@users.noreply.github.com>
2024-04-09 16:31:52 -0300

    Update Estacionamento.cs

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,10 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DesafioFundamentos.Models
 {
+    public class VeiculoEstacionado
+    {
+        public string Placa { get; }
+        public DateTime HorarioEntrada { get; }
+
+        public VeiculoEstacionado(string placa)
+        {
+            Placa = placa;
+            HorarioEntrada = DateTime.Now;
+        }
+    }
+
     public class Estacionamento
     {
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private List<VeiculoEstacionado> veiculosEstacionados = new List<VeiculoEstacionado>();
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
@@ -14,49 +30,43 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine();
+            VeiculoEstacionado novoVeiculo = new VeiculoEstacionado(placa);
+            veiculosEstacionados.Add(novoVeiculo);
+            Console.WriteLine($"Veículo com placa {placa} estacionado com sucesso.");
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine();
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
-
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            VeiculoEstacionado veiculo = veiculosEstacionados.FirstOrDefault(x => x.Placa.ToUpper() == placa.ToUpper());
+            if (veiculo != null)
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
+                DateTime horarioSaida = DateTime.Now;
+                TimeSpan tempoEstacionado = horarioSaida - veiculo.HorarioEntrada;
+                decimal valorTotal = precoInicial + (precoPorHora * (decimal)tempoEstacionado.TotalHours);
+                veiculosEstacionados.Remove(veiculo);
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente.");
             }
         }
 
         public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+            if (veiculosEstacionados.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                foreach (var veiculo in veiculosEstacionados)
+                {
+                    TimeSpan tempoEstacionado = DateTime.Now - veiculo.HorarioEntrada;
+                    Console.WriteLine($"Placa: {veiculo.Placa}, Tempo estacionado: {tempoEstacionado}");
+                }
             }
             else
             {
